384fb821bcd339c0e5a73337e8d2568863bb9685
bucceroni <bucceroni@hotmail.com>
2019-12-02 17:21:40 -0300

    start project

---
+++ packages/app/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Another from "components/lib/Another";
+import Hello from "components/lib/Hello";
+
+const App = () => {
+  return (
+    <div>
+      <Another />
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/components/src/Another.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+function Another() {
+  return <div>Another Component</div>;
+}
+export default Another;
